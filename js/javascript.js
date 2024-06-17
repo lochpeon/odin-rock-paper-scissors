7b5c777eb1b64ca763888c04421dc95e03e676fc
lochpm <163648572+lochpeon@users.noreply.github.com>
2024-06-17 13:17:55 +0800

    Create getHumanChoice function

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,18 @@ function getComputerChoice() {
 function getRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
+
+function getHumanChoice() {
+    let choice = prompt("Choose between Rock, Paper, or Scissors", "Rock");
+    switch (choice) {
+        case "Rock":
+        case "rock":
+            return "Rock";
+        case "Paper":
+        case "paper":
+            return "Paper";
+        case "Scissors":
+        case "scissors":
+            return "Scissors";
+    }
+}
