5fa2c0434305c31593a312c2021d500dda89de39
lochpm <163648572+lochpeon@users.noreply.github.com>
2024-07-16 21:53:04 +0800

    Remove getHumanChoice, getRandomChoice
    Remove getHumanChoice for redundancy, as the choice comes from the user clicking his/her choice from the three buttons/choices
    
    Rename getRandomChoice to getComputerChoice, as the latter just calls the former, making it redundant

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,8 @@
-function getComputerChoice() {
-    return getRandomChoice();
-}
-
 function getRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
 
-function getRandomChoice() {
+function getComputerChoice() {
     let choice = getRandomNumber();
     switch (choice) {
         case 0: 
@@ -14,18 +10,6 @@ function getRandomChoice() {
         case 1:
             return "Paper";
         case 2: 
-            return "Scissors";
-    }
-}
-
-function getHumanChoice() {
-    // let choice = prompt("Choose between Rock, Paper, or Scissors", getRandomChoice());
-    switch (choice.toLowerCase()) {
-        case "rock":
-            return "Rock";
-        case "paper":
-            return "Paper";
-        case "scissors":
             return "Scissors";
     }
 }
